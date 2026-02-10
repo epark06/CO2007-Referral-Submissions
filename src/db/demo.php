d0d9702841a656bbbfd5f7d249a6807698a04b14
Eoin Park <eoinjpark@gmail.com>
2026-02-10 13:16:30 +0000

    Updated demo.php - Provided sample data to insert into the referral table to assess functionality

--- src/db/demo.php
+++ src/db/demo.php
@@ -3,8 +3,7 @@
 
 declare(strict_types=1);
 
-require_once("Database.php");
-require_once("DatabaseSetup.php");
+require_once("autoload.php");
 
 // Initialises a Database instance and retrieves the PDO connection
 $db = new Database();
@@ -12,7 +11,7 @@ $pdo = $db->getConnection();
 
 echo "<h1>SCHEMA INIT</h1>";
 
-// Creates the co2717_clothing, co2717_clothing_sizes and co2717_warehouses tables
+// Creates the co2007_referral table
 $setup = new DatabaseSetup($pdo);
 // Only truthy in event of failure
 $schemaFail = $setup->createDatabaseSchema();
@@ -20,9 +19,24 @@ $schemaFail = $setup->createDatabaseSchema();
 // If any of these tables' queries failed, throw a runtime exception
 if($schemaFail) {
     // Generates error message, pluralising if multiple tables failed to be created
-    $pluralHandler = $schemaFail . (str_contains($schemaFail,", ") ? " have" : " has");
+    $pluralHandler = $schemaFail . (str_contains($schemaFail," and ") ? " have" : " has");
     $e = $pluralHandler . " failed to generate, aborting program.";
     throw new RuntimeException($e);
 }
+
+$repo = new ReferralRepository($pdo);
+$referralData = [
+    "referral_id" => 17564121,
+    "physician_name" => "Jane Doe",
+    "clinic_name" => "St. Mary's Clinic",
+    "patient_phone_no" => "07000 560000",
+    "patient_email" => "john-doe@gmail.com",
+    "fax_no" => "0161 000 0000",
+    "patient_name" => "John Doe",
+    "patient_dob" => "1999-01-01",
+    "urgency_level" => "Routine",
+    "referral_reason" => "Patient complaing of shortness of breath"
+];
+$repo->insertReferralData($referralData);
 
 ?>
